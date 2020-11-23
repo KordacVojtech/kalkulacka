e54bb9cc79adc0f968dd3139649c976006529723
KordacVojtech <74076141+KordacVojtech@users.noreply.github.com>
2020-11-23 10:59:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const display = document.getElementById('display');
 const equals = document.getElementById('equals')
 const clear = document.getElementById('clear');
 const procent = document.getElementById('procent');
+const plusMinus = document.getElementById('plusMinus');
 
 for (let i=0; i<btns.length; i++){
     btns[i].addEventListener('click', function(){
@@ -25,11 +26,18 @@ procent.addEventListener('click', function(){
     let value = eval((display.value)/100);
     display.value = value;
 })
-    
 
-    
-    
-
+plusMinus.addEventListener('click', function(){
+    let value = display.value;
+    if(value[0]=='-'){
+        value=value.substring(1);
+        display.value = value;
+    } else{
+        let minus = '-';
+        value = minus.concat(display.value);
+        display.value = value;
+    }
+})
     
 
     
